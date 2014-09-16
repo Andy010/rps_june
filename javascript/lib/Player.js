66ad150f49de6616ca80a2b826684c29f028b91c
Andy010 <andy_gates_010@hotmail.co.uk>
2014-09-16 18:45:12 +0100

    Adding a randomizer

--- javascript/lib/Player.js
+++ javascript/lib/Player.js
@@ -5,3 +5,8 @@ function Player(name) {
 Player.prototype.picks = function(pick) {
   this.pick = pick;
 }
+
+Player.prototype.randomlyPicks = function(array) {
+	this.pick = array[Math.floor( Math.random() * array.length )];
+	return this.pick;
+};
---
+++ javascript/lib/interface.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+  var p1 = new Player('Andy');
+  var p2 = new Player('Nick');
+  var game = new Game(p1, p2);
+
+  $('.choices img').on('click', function(){
+    p1.picks($(this).data('pick'));
+    p2.picks('rock');
+
+  var choices = $('.choices img').map(function(){ 
+    return $(this).data('pick');
+  });
+
+  p1.randomlyPicks(choices);
+  p2.randomlyPicks(choices);
+
+// $('#results').prepend('<li>' + game.winningMessage() + '</li>');
+    $("<li>" + game.winningMessage() + "</li>").prependTo('#results').slideDown();
+
+    $("#results li:gt(2)").slideToggle(function(){
+      $(this).remove();
+      console.log('A')
+    });
+    console.log('B')
+
+  })
+})
